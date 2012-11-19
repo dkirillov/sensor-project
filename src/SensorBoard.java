080d43f2ab2bcb254575313907ea55b0b2f3601a
Wesley-Lawrence <wes@transpose-games.com>
2012-11-19 10:51:13 -0500

    Updated some calculations

--- src/SensorBoard.java
+++ src/SensorBoard.java
@@ -1,8 +1,10 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JPanel;
 
@@ -11,7 +13,7 @@ public class SensorBoard extends JPanel {
 	public SensorBoard(){
 		setVisible(true);
 	}
-	public void draw(Graphics gfx, Sensor[] sensors){
+	public void draw(Graphics gfx, Sensor[] sensors, Vector<Neighbour> neighbours){
 		BufferedImage buffer = new BufferedImage(598, 438, BufferedImage.TYPE_INT_RGB);
 		Graphics graphics = buffer.getGraphics();
 		
@@ -20,6 +22,15 @@ public class SensorBoard extends JPanel {
 
 		//Just testing if all neighbours are discovered
 		graphics.setColor(Color.RED);
+		for (Neighbour n : neighbours) {
+			
+			if(!n.isConnected()) continue;
+			
+			Point p1 = n.getSensor1Pos();
+			Point p2 = n.getSensor2Pos();
+			graphics.drawLine(p1.x, p1.y, p2.x, p2.y);
+		}
+		/*graphics.setColor(Color.RED);
 		for(int x = 0;x<sensors.length;x++){
 			int start_x = sensors[x].getPoint().x;
 			int start_y = sensors[x].getPoint().y;
@@ -32,7 +43,7 @@ public class SensorBoard extends JPanel {
 				//System.out.println("start_x: "+start_x+" start_y: "+start_y);
 				graphics.drawLine(start_x, start_y, end_x, end_y);
 			}
-		}		
+		}*/
 		
 		for(int x = 0 ;x <sensors.length;x++){
 			sensors[x].draw(graphics);
